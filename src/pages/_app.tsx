fcd80a6a9518ffb099d0ec844aafc40755ba3929
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-10-24 19:55:16 -0300

    style(header): adjust header styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
-import { globalStyles } from '@/styles/global'
+/* eslint-disable @next/next/no-img-element */
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
+
+import logoimg from "../assets/logo.svg"
+
+import { Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+
+      <Header>
+        <img src={logoimg.src} alt="logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
