602f5260177894a1612382a42e96b360d941053d
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-10-24 23:41:22 -0300

    feat(home): Add carousel to the home page

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -4,17 +4,22 @@ export const HomeContainer = styled('main',{
     display:'flex',
     gap:'3rem',
     width:'100%',
-    maxWidth:'calc(100vw - ((100vw - 1180) / 2 ))',
-    marginLeft:'auto',
+
+    maxWidth:'calc(100vw - ((100vw - 1180px) / 2 ))',
     minHeight:'656px',
+
+    marginLeft:'auto',
 })
 
 export const Product = styled('a',{
+    position:'relative',
+    padding:'0.25rem',
+
     background:'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)', 
     borderRadius:8,
-    padding:'0.25rem',
+
     cursor: 'pointer',
-    position:'relative',
+
     display:'flex',
     alignItems:'center',
     justifyContent:'center',
