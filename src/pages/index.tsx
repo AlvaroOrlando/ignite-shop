08d26182a87ccccd2901f7e3bd2092360479edfb
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-10-25 03:12:36 -0300

    style(home): Format product prices as BRL (pt-BR)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,17 +61,20 @@ export const getStaticProps:GetStaticProps = async () => {
 
   const products = response.data.map(product => {
 
-  const price = product.default_price as Stripe.Price
+    const price = product.default_price as Stripe.Price
 
-    
-  if (price) {
-    return {
-      id: product.id,
-      name: product.name,
-      imageUrl: product.images[0],
-      price: price.unit_amount ? price.unit_amount / 100 : 0, 
-    };
-  }
+      
+    if (price) {
+      return {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0)
+      };
+    }
 
   })
 
