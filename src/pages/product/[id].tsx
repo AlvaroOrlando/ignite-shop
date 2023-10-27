840e99eeb74166ce6890dbbce4d5d6afbeeb87a4
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-10-27 01:37:45 -0300

    feat(product): Implement product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product(){
 
 const {query} = useRouter()
     return (
-        <h1>Product{JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,00</span>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum fuga in nihil magni itaque reiciendis maxime rerum! 
+                    Suscipit, ab, asperiores, architecto quibusdam laboriosam eligendi nisi quod quae at maxime incidunt.
+                </p>
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,74 @@
+import { styled } from "@stitches/react";
+
+export const ProductContainer = styled('main', {
+    display:'grid',
+    gridTemplateColumns:'1fr 1fr',
+    alignItems:'stretch',
+    gap:'4rem',
+
+    maxWidth:'1180px',
+    margin:'0px auto'
+
+})
+
+export const ImageContainer = styled('div', {
+    width:'100%',
+    maxWidth:'576px',
+    height:'calc(656px - 0.65rem)',
+    background:'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)', 
+    borderRadius:'8px',
+    padding:'0.25rem',
+
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+
+    img: {
+        objectFit:'cover'
+    }
+})
+
+export const ProductDetails = styled('div', {
+    display:'flex',
+    flexDirection:'column',
+
+    h1: {
+        fontSize:'$2xl',
+        color:'$gray300'
+    },
+
+    span: {
+        marginTop:'1rem',
+        display:'block',
+        fontSize:'$2xl',
+        color:'$green300'
+    },
+
+    p:{
+        marginTop:'2.5rem',
+        fontSize:'$md',
+        lineHeight:'1.6',
+        color:'$gray300'
+    },
+
+    button:{
+        marginTop:'auto',
+        padding:'1.25rem',
+
+        border:'0px',
+        borderRadius:'8px',
+
+        backgroundColor:'$green500',
+        color:'$white',
+
+        cursor:'pointer',
+
+        fontSize:'$md',
+        fontWeight:'bold',
+
+        '&:hover': {
+            backgroundColor:'$green300'
+        },
+    }
+})
+
