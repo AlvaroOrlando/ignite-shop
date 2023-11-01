3cb57f11839269c55f73168e7d0fa8a43265d940
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-11-01 15:59:23 -0300

    style(success): Create visual structure for success page

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,5 +1,23 @@
+
+import { SuccessContainer, SuccessImageContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success(){
     return (
-        <h1>Success</h1>
+       <SuccessContainer>
+        <h1>Compra efetuada!</h1>
+
+        <SuccessImageContainer>
+
+        </SuccessImageContainer>
+
+        <p>
+            Uhull, <strong>Alvaro Orlando</strong>, sua camiseta <strong>Ignite</strong> já está a caminho da sua casa!
+        </p>
+
+        <Link href="/">
+            Voltar ao catálogo
+        </Link>
+       </SuccessContainer>
     )
 }
