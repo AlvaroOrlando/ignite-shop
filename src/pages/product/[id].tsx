9fbe28c4c60aa8c683c8957def6f2ee908f92372
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-11-01 00:24:45 -0300

    feat(checkout): Create 'disabled' property for purchase button, implement 'handleBuyProduct' function, finalize purchase API, and style button during Stripe API call

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
 import { stripe } from '@/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
+import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Stripe from 'stripe'
 
 interface ProductProps {
@@ -18,8 +20,29 @@ interface ProductProps {
 
 export default function Product({ product }:ProductProps){
 
-    function handleBuyProduct(){
-        console.log(product.defaultPriceId);
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession ] = useState(false)
+
+    async function handleBuyProduct(){
+        try {
+
+            setIsCreatingCheckoutSession(true)
+
+            const response = await axios.post('/api/checkout', {
+                priceId:product.defaultPriceId
+            })
+
+            const { checkoutUrl } = response.data;
+
+            //rota externa (para usar rota interna, usar router.push())
+            window.location.href = checkoutUrl
+
+        } catch(err) {
+
+            setIsCreatingCheckoutSession(false)
+
+            //Conectar com alguma ferramenta de observabilidade (Datadog / Sentry)
+            alert('Falha ao redirecionar ao checkout')
+        } 
     }
 
     const { isFallback } = useRouter()
@@ -40,7 +63,7 @@ export default function Product({ product }:ProductProps){
                 <p>
                     {product.description}
                 </p>
-                <button onClick={handleBuyProduct}>Comprar agora</button>
+                <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>Comprar agora</button>
             </ProductDetails>
         </ProductContainer>
     )
